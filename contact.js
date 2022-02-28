b96aadc6177888cc486da6171dc5c1653e80af9e
Guillaume Ducuing <86024187+ducuingg@users.noreply.github.com>
2022-02-28 01:07:49 +0100

    n

--- contact.js
+++ contact.js
@@ -1,6 +1,7 @@
 
 // ANIMATION 
 const barreform = document.querySelector(".barreform");
+const map = document.querySelector("iframe");
 
 window.addEventListener('load',()=>{
 
@@ -8,5 +9,6 @@ window.addEventListener('load',()=>{
 
 TL1
 .from(barreform,2.5,{width:0,ease:"power1.out"})
+.from(map,3,{y:60,opacity:0}, '-=2')
 TL1.play();
 })
--- main.js
+++ main.js
@@ -3,13 +3,17 @@
 const logo = document.querySelector(".logo");
 const slidehome = document.querySelector(".slidehome");
 const h1 = document.querySelector("h1");
+const barreform = document.querySelector(".barreform");
+const map = document.querySelector("iframe");
 
 const TL1 = new TimelineMax({paused:true});
 
 TL1
 .staggerFrom(logo,1,{y:-100,opacity:0},0.3)
-.staggerFrom(slidehome,1,{y:340,opacity:0},0.3,'-=0.8')
+.staggerFrom(slidehome,1,{y:40,opacity:0},0.3,'-=0.8')
 .from(h1,2.5,{width:0,ease:"power1.out"})
+.from(barreform,2.5,{width:0,ease:"power1.out"})
+.from(map,3,{y:60,opacity:0}, '-=2')
 //.from(slidehome,1,{y:-40,opacity:100}, '-=0.5')
 
 TL1.play();
