bf1d67af57a21a47d7741f052d38d72c86fa49d6
Guillaume Ducuing <86024187+ducuingg@users.noreply.github.com>
2022-03-08 15:58:45 +0100

    animation

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // ANIMATION 
 const logo = document.querySelector(".logo");
 const slidehome = document.querySelector(".slidehome");
+const iconscroll = document.querySelector(".icon-scroll");
 const barreformprojet = document.querySelector(".barreformprojet");
 const iconehand = document.querySelector(".iconehand");
 const iconeprojet = document.querySelector(".iconeprojet");
@@ -14,7 +15,8 @@ const TL1 = new TimelineMax({paused:true});
 
 TL1
 .staggerFrom(logo,1,{y:-100,opacity:0},0.3)
-.staggerFrom(slidehome,2,{y:40,opacity:0},0.3,'-=0.8')
+.staggerFrom(slidehome,1.5,{y:40,opacity:0},0.3,'-=0.8')
+.staggerFrom(iconscroll,1,{y:40,opacity:0},0.3,'-=0.8')
 .from(barreformprojet,2.5,{width:0,ease:"power1.out"})
 .from(barreformusine,2.5,{width:0,ease:"power1.out"})
 .from(barreformcontact,2.5,{width:0,ease:"power1.out"})
