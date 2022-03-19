edd4dc4792a6dd80013a5221c012d6386db7690f
ducuingg <ducuingg@yahoo.fr>
2022-03-19 23:22:45 +0100

    galerie

--- main.js
+++ main.js
@@ -8,20 +8,21 @@ const iconehand = document.querySelector(".iconehand");
 const iconeprojet = document.querySelector(".iconeprojet");
 const iconedraw = document.querySelector(".iconedraw");
 const barreformusine = document.querySelector(".barreformusine");
+const barreformreal = document.querySelector(".barreformreal");
 const barreformcontact = document.querySelector(".barreformcontact");
 const map = document.querySelector("iframe");
-
 const TL1 = new TimelineMax({paused:true});
 
 TL1
 .staggerFrom(logo,1,{y:-100,opacity:0},0.3)
 .staggerFrom(slidehome,1.5,{y:40,opacity:0},0.3,'-=0.8')
 .staggerFrom(iconscroll,1,{y:40,opacity:0},0.3,'-=0.8')
-.from(barreformprojet,2.5,{width:0,ease:"power1.out"})
-.from(barreformusine,2.5,{width:0,ease:"power1.out"})
-.from(barreformcontact,2.5,{width:0,ease:"power1.out"})
-.from(map,3,{y:60,opacity:0}, '-=2')
-//.from(slidehome,1,{y:-40,opacity:100}, '-=0.5')
+.from(barreformprojet,2,{width:0,ease:"power1.out"})
+.from(barreformusine,2,{width:0,ease:"power1.out"})
+.from(barreformreal,2,{width:0,ease:"power1.out"})
+.from(barreformcontact,2,{width:0,ease:"power1.out"})
+.from(map,2,{y:60,opacity:0}, '-=2')
+
 TL1.play();
 
 /*window.addEventListener('scroll',()=>{
